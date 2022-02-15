30f5039b2c9ff719683bc7e21152e08420f0f83c
Hector Javier Moreno <cytelsystem@gmail.com>
2022-02-15 14:18:51 -0500

    adicionando cambios

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Principal from '../pages/Principal';
 import JavaScriptNode from '../pages/JavaScriptNode';
@@ -9,13 +9,13 @@ import Mysql from '../pages/Mysql';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
           <Route exact path="/JavaScriptNode" component={JavaScriptNode} />
           <Route exact path="/Mysql" component={Mysql} />
           <Route exact path="/" component={Principal} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
