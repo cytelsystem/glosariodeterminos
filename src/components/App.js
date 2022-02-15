e1261accaff64e8204df05a99f6209f93deeac2f
Hector Javier Moreno <cytelsystem@gmail.com>
2022-02-15 14:49:39 -0500

    adicionando

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,10 @@ import Mysql from '../pages/Mysql';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL + '/'}>
       <Switch>
           <Route exact path="/JavaScriptNode" component={JavaScriptNode} />
-          <Route exact path="/Mysql" component={Mysql} />
+          <Route exact path="/" component={Mysql} />
           <Route exact path="/" component={Principal} />
       </Switch>
     </Router>
