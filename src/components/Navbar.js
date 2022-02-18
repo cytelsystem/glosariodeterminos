5cf04557ecb747c1bdcbddeea375c8a06a7f2e74
Hector Javier Moreno <cytelsystem@gmail.com>
2022-02-17 20:46:08 -0500

    corrigiendo el offcanvas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import data from '../db.json';
 import './styles/navbar.scss';
+
 import * as ReactBootStrap from "react-bootstrap";
 
 function Navbar() {
